92e074f93527df2ee8363cbc2a7f11936d980a59
Evin <80323753+evindag@users.noreply.github.com>
2023-03-07 15:26:23 +0300

    Add files via upload

---
+++ swing/Jlist/src/main/java/com/mycompany/jlist/Jlist.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.jlist;
+
+public class Jlist {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ swing/firstSwingDemo/src/main/java/com/mycompany/firstswingdemo/FirstSwingDemo.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.firstswingdemo;
+
+public class FirstSwingDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
